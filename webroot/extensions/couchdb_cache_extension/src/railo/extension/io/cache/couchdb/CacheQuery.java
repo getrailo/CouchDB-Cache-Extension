e4624da829503ea9e4920ce244d7c0accb5abb96
Mark Drew <mark.drew@gmail.com>
2010-07-20 13:36:11 +0100

    New Classes for the converter

---
+++ webroot/extensions/couchdb_cache_extension/src/railo/extension/io/cache/couchdb/CacheQuery.java
@@ -0,0 +1,17 @@
+package railo.extension.io.cache.couchdb;
+
+public class CacheQuery {
+
+	private String name;
+	public CacheQuery() {
+		super();
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+}
---
+++ webroot/extensions/couchdb_cache_extension/src/railo/extension/io/cache/couchdb/QueryConverer.java
@@ -0,0 +1,23 @@
+package railo.extension.io.cache.couchdb;
+
+import org.svenson.converter.TypeConverter;
+
+public class QueryConverer implements TypeConverter {
+
+	
+	 public QueryConverer()
+	    {
+	       
+	    }
+	
+	public Object fromJSON(Object in) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object toJSON(Object in) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
